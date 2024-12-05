6641a79f0c2db0cb384f5b46fcf9ccc3556fdc3a
Hyejin Kim <tamanta713@gmail.com>
2024-12-05 15:15:00 +0900

    Update todolist.jsx

---
+++ todo/src/component/todolist.jsx
@@ -0,0 +1,22 @@
+// TodoList.jsx
+/* eslint-disable react/prop-types */
+import TodoItem from './TodoItem';
+import style from "../css/TodoList.module.css";
+
+const TodoList = ({ todos, updateTodo, toggleComplete, deleteTodo }) => {
+    return (
+        <ul className={style.ul}>
+        {todos.map((todo) => (
+            <TodoItem
+            key={todo.id}
+            todo={todo}
+            updateTodo={updateTodo}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            />
+        ))}
+        </ul>
+    );
+};
+
+export default TodoList;
